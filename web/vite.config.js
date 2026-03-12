8dee1d85091fc7ee713211b1db2fd53c5cd3cd52
Yp <you@example.com>
2026-03-12 19:31:04 +0800

    update

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
+    port: 5174,
     allowedHosts: ['.natappfree.cc', '.natapp.cc'],
     historyApiFallback: true,
     proxy: {
